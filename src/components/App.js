bea02aea9ebed68f18feb91801435d789ef588b0
mani <manikumarjammula999@gmail.com>
2023-08-18 01:22:21 +0530

    adding data and creating movie app UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
-
-
+import React from 'react';
+import { data } from '../data';
+import NavBar from './NavBar';
+import MovieCard from './MovieCard';
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-       Movie App
-    </div>
+      <NavBar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+          <div className="list">
+            {data.map(movie => (
+              
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        </div>
+      </div>
   );
 }
 
