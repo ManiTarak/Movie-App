f4e8919459a787cc252a73254adfd80cd81d87d7
mani <manikumarjammula999@gmail.com>
2023-10-06 10:05:54 +0530

    redux middleware

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createStore} from 'redux';
+import { applyMiddleware } from 'redux';
 import App from './components/App';
 import rootreducer from './reducers/index'
 import './index.css'
 
-const store=createStore(rootreducer);
+const logger=function ({dispatch,getState}){
+  return function (next){
+    return function(action){
+      console.log("Action Type =  ",action.type);
+      next(action);
+    }
+  }
+}
+const secondmiddleware=function ({dispatch,getState}){
+  return function (next){
+    return function(action){
+      console.log("this second middleware");
+     next(action);
+    }
+  }
+}
+const store=createStore(rootreducer,applyMiddleware(logger,secondmiddleware));
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
